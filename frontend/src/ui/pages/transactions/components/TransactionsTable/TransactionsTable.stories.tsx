554faa43a591037fc428a464669d90ff65f529ea
lumo <lmotowid@gmail.com>
2022-12-14 22:29:28 +0100

    Add table + tests (save work for tomorrow)

--- frontend/src/ui/pages/transactions/components/TransactionsTable/TransactionsTable.stories.tsx
+++ frontend/src/ui/pages/transactions/components/TransactionsTable/TransactionsTable.stories.tsx
@@ -1,11 +1,65 @@
-import { TransactionsTable } from './TransactionsTable'
-import { ComponentStoryObj } from '@storybook/react'
+import { TransactionsTable } from "./TransactionsTable";
+import { ComponentStoryObj } from "@storybook/react";
+import { transactionsTableMocks } from "./TransactionsTable.mocks";
+import { within } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
 
-type StoryType = ComponentStoryObj<typeof TransactionsTable>
+type StoryType = ComponentStoryObj<typeof TransactionsTable>;
 export default {
-  component: TransactionsTable
-}
+  component: TransactionsTable,
+  args: {
+    isLoading: false,
+  },
+};
 
 export const Default: StoryType = {
+  args: {
+    transactions: transactionsTableMocks.transactions,
+  },
+  play: async (context) => {
+    const canvas = within(context.canvasElement);
 
-}
+    await canvas.findByTestId("transaction-table-head");
+
+    const rows = await canvas.findAllByTestId("transaction-table-row");
+
+    expect(rows).toHaveLength(2);
+  },
+};
+
+export const OneRow: StoryType = {
+  args: {
+    transactions: [transactionsTableMocks.transactions[0]],
+  },
+  play: async (context) => {
+    const expectedCellValues = [
+      0,
+      -2008.75,
+      "Callie Nieves",
+      "PL10104092290785174000000000",
+      "185 Berkeley Place, Brady, West Virginia, 7409",
+      "15.12.2015 01:05:42",
+      "Amet amet qui proident sint esse adipisicing amet.",
+    ];
+
+    const canvas = within(context.canvasElement);
+
+    const row = await canvas.findByTestId("transaction-table-row");
+
+    Array.from(row.children).forEach((cell, index) => {
+      expect(cell).toHaveTextContent(String(expectedCellValues[index]));
+    });
+  },
+};
+
+export const Loading: StoryType = {
+  args: {
+    isLoading: true,
+  },
+};
+
+export const Empty: StoryType = {
+  args: {
+    transactions: [],
+  },
+};
--- frontend/src/ui/pages/transactions/components/TransactionsTable/TransactionsTable.tsx
+++ frontend/src/ui/pages/transactions/components/TransactionsTable/TransactionsTable.tsx
@@ -1,4 +1,78 @@
+import {
+  Paper,
+  Table,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TablePagination,
+  TableRow,
+} from "@mui/material";
+import { Transaction } from "types/types";
+import { useState } from "react";
 
-export const TransactionsTable = (): JSX.Element | null => {
-  return null
+interface TransactionTableProps extends BodyProps {
+  isLoading: boolean;
 }
+export const TransactionsTable = ({ transactions }: TransactionTableProps): JSX.Element | null => {
+  const [page, setPage] = useState(20);
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label={"transactions table"}>
+          <Head />
+          <Body transactions={transactions} />
+        </Table>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[20, 40]}
+              rowsPerPage={page}
+              count={transactions.length}
+              page={page}
+              onPageChange={(_, pageNumber) => setPage(pageNumber)}
+            />
+          </TableRow>
+        </TableFooter>
+      </TableContainer>
+    </>
+  );
+};
+
+const Head = (): JSX.Element => {
+  return (
+    <TableHead>
+      <TableRow data-testid={"transaction-table-head"}>
+        <TableCell>ID</TableCell>
+        <TableCell align="right">Amount</TableCell>
+        <TableCell align="right">Beneficiary</TableCell>
+        <TableCell align="right">Account number</TableCell>
+        <TableCell align="right">Address</TableCell>
+        <TableCell align="right">Date</TableCell>
+        <TableCell align="right">Description</TableCell>
+      </TableRow>
+    </TableHead>
+  );
+};
+
+interface BodyProps {
+  transactions: Transaction[];
+}
+const Body = ({ transactions }: BodyProps): JSX.Element => {
+  return (
+    <>
+      {transactions.map((row) => (
+        <TableRow data-testid={"transaction-table-row"} key={row.id}>
+          <TableCell align="right">{row.id}</TableCell>
+          <TableCell align="right">{row.amount}</TableCell>
+          <TableCell align="right">{row.beneficiary}</TableCell>
+          <TableCell align="right">{row.account}</TableCell>
+          <TableCell align="right">{row.address}</TableCell>
+          <TableCell align="right">{row.date}</TableCell>
+          <TableCell align="right">{row.description}</TableCell>
+        </TableRow>
+      ))}
+    </>
+  );
+};
