4bacc5d55f2509af669faccbfbf72dd50213f932
lumo <lmotowid@gmail.com>
2022-12-15 08:32:40 +0100

    Slight table refactor

--- frontend/src/ui/pages/transactions/components/TransactionsTable/TransactionsTable.tsx
+++ frontend/src/ui/pages/transactions/components/TransactionsTable/TransactionsTable.tsx
@@ -1,6 +1,7 @@
 import {
   Paper,
   Table,
+  TableBody,
   TableCell,
   TableContainer,
   TableFooter,
@@ -11,43 +12,6 @@ import {
 } from "@mui/material";
 import { Transaction } from "types/types";
 import { useState } from "react";
-
-interface TransactionTableProps extends BodyProps {
-  isLoading: boolean;
-}
-export const TransactionsTable = ({ transactions }: TransactionTableProps): JSX.Element | null => {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(20);
-
-  const onPageChangeHandler: TablePaginationProps["onPageChange"] = (_, pageNumber) => setPage(pageNumber);
-  const onRowsPerPageChangeHandler: TablePaginationProps["onRowsPerPageChange"] = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-
-  return (
-    <>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label={"transactions table"}>
-          <Head />
-          <Body transactions={transactions} />
-        </Table>
-        <TableFooter>
-          <TableRow>
-            <TablePagination
-              rowsPerPageOptions={[20, 40]}
-              rowsPerPage={rowsPerPage}
-              onRowsPerPageChange={onRowsPerPageChangeHandler}
-              count={transactions.length}
-              page={page}
-              onPageChange={onPageChangeHandler}
-            />
-          </TableRow>
-        </TableFooter>
-      </TableContainer>
-    </>
-  );
-};
 
 const Head = (): JSX.Element => {
   return (
@@ -70,9 +34,9 @@ interface BodyProps {
 }
 const Body = ({ transactions }: BodyProps): JSX.Element => {
   return (
-    <>
+    <TableBody>
       {transactions.map((row) => (
-        <TableRow data-testid={"transaction-table-row"} key={row.id}>
+        <TableRow data-testid={"transaction-table-row"} key={row.date}>
           <TableCell align="right">{row.id}</TableCell>
           <TableCell align="right">{row.amount}</TableCell>
           <TableCell align="right">{row.beneficiary}</TableCell>
@@ -82,6 +46,43 @@ const Body = ({ transactions }: BodyProps): JSX.Element => {
           <TableCell align="right">{row.description}</TableCell>
         </TableRow>
       ))}
+    </TableBody>
+  );
+};
+
+interface TransactionTableProps extends BodyProps {
+  isLoading: boolean;
+}
+export const TransactionsTable = ({ transactions }: TransactionTableProps): JSX.Element | null => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(20);
+
+  const onPageChangeHandler: TablePaginationProps["onPageChange"] = (_, pageNumber) => setPage(pageNumber);
+  const onRowsPerPageChangeHandler: TablePaginationProps["onRowsPerPageChange"] = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label={"transactions table"}>
+          <Head />
+          <Body transactions={transactions} />
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[20, 40]}
+                rowsPerPage={rowsPerPage}
+                onRowsPerPageChange={onRowsPerPageChangeHandler}
+                count={transactions.length}
+                page={page}
+                onPageChange={onPageChangeHandler}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
     </>
   );
 };
